963297e71e9d695f2f9c81f0d7e1ab0af449e855
AoranZhao <aoran_zhao@yahoo.ca>
2017-10-26 07:51:59 -0400

    first edition

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var path = require('path');
+
+var port = 2980;
+
+let app = express();
+
+app.use(express.use(path.join(__dirname, 'public')));
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+})
